181acedcfae1a1f68e41cbc67df71b3a1a5715e4
lromang <luis.roangarci@gmail.com>
2017-07-11 13:51:46 -0500

    polygon

--- huracanes/src/mapPoints.sh
+++ huracanes/src/mapPoints.sh
@@ -30,7 +30,8 @@ echo   'map.addLayer({
         "id": "dangerZone","type": "fill","source": {"type": "geojson", "data": {"type": "Feature","geometry": {"type": "Polygon","coordinates":[[' >> ../html/body.html
 
 ## Add body of polygon
-lastUpdate="https://correo1.conagua.gob.mx/Feedsmn16/"$(curl https://correo1.conagua.gob.mx/feedsmn/feedalert.aspx | grep '<id>' | grep '.*avisossmn.*' | head -n 1 | grep -Eo '>.*<' | sed -r 's/(<|>)//g')"_cap.xml"
+## lastUpdate="https://correo1.conagua.gob.mx/Feedsmn16/"$(curl https://correo1.conagua.gob.mx/feedsmn/feedalert.aspx | grep '<id>' | grep '.*avisossmn.*' | head -n 1 | grep -Eo '>.*<' | sed -r 's/(<|>)//g')"_cap.xml"
+lastUpdate="https://correo1.conagua.gob.mx/Feedsmn16/avisossmn-ciclontropical-2127_cap.xml"
 polygon=$(curl $lastUpdate | grep -o '<polygon>.*</polygon>'| grep -Eo '>.*<' | sed -r 's/(<|>)//g')
 echo $polygon | sed 's/ /\n/g' | awk -F ',' '{print "["$2","$1"],"}' >> ../html/body.html
 ## Remove last comma
